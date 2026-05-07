c664ddf623360063532161e0b12ed8492eea1128
Miarintsoa Christian <ainafinaritra17@gmail.com>
2026-05-07 15:08:48 +0300

    Member.java

--- src/main/java/com/federation/entities/Member.java
+++ src/main/java/com/federation/entities/Member.java
@@ -1,11 +1,15 @@
 package com.federation.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
     private String id;
     private String firstName;
