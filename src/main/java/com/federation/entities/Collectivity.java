ec6a2ea80d88b1bd9c26e1f8f1ae818730291dd4
Miarintsoa Christian <ainafinaritra17@gmail.com>
2026-05-07 15:08:03 +0300

    Collectivity.java

--- src/main/java/com/federation/entities/Collectivity.java
+++ src/main/java/com/federation/entities/Collectivity.java
@@ -1,10 +1,14 @@
 package com.federation.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Collectivity {
     private String id;
     private String name;
